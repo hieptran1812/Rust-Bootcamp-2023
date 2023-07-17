15a94cdf91e539d34879d7bc6eb5c0b632627cb8
hieptran1812 <hieptran.jobs@gmail.com>
2023-07-17 21:55:11 +0700

    feat: add exe 2

--- exercises/ownership-borrowing/src/lib.rs
+++ exercises/ownership-borrowing/src/lib.rs
@@ -1,11 +1,11 @@
 // Exercise 1
 // Make it compile
 fn exercise1() {
-    // Use as many approaches as you can to make it work
     let x = String::from("hello, world");
-    let y = x;
-    let z = x;
+    let y = x.clone();
+    let z = x.clone();
 }
+
 
 // Exercise 2
 // Make it compile
@@ -15,10 +15,11 @@ fn exercise2() {
     let s2 = take_ownership(s1);
 
     println!("{}", s2);
-}
+}   
 // Only modify the code below!
-fn take_ownership(s: String) {
+fn take_ownership(s: String) -> String{
     println!("{}", s);
+    s
 }
 
 // Exercise 3
@@ -33,32 +34,37 @@ fn exercise3() {
 
     let values_number = values.len();
 
-    let additions: Vec<usize> = vec![0];
+    let additions: Vec<usize> = vec![]; // Initialize with an empty vector
 
     println!("{:?}", values_number);
 
     while additions.len() > 0 {
         let mut addition: f64 = 0.0;
 
-        // Sumar valores en additions
-        for element_index in additions {
+        // Sum values in additions
+        for &element_index in &additions { // Add '&' before 'additions' to borrow the values
             let addition_aux = values[element_index];
             addition = addition_aux + addition;
         }
+
+        // Break the loop or modify the additions vector
+        break;
     }
 }
+
 
 // Exercise 4
 // Make it compile
 fn exercise4(value: u32) -> &'static str {
-    let str_value = value.to_string(); // Convert u32 to String
-    let str_ref: &str = &str_value; // Obtain a reference to the String
-    str_ref // Return the reference to the String
+    let str_value = value.to_string();
+    let static_str: &'static str = Box::leak(str_value.into_boxed_str());
+    static_str
 }
 
 // Exercise 5
 // Make it compile
 use std::collections::HashMap;
+
 fn exercise5() {
     let mut my_map = HashMap::from([(1, "1.0".to_string()), (2, "2.0".to_string())]);
 
@@ -68,64 +74,40 @@ fn exercise5() {
         Some(child) => child,
         None => {
             let value = "3.0".to_string();
-            my_map.insert(key, value);
-            &value // HERE IT FAILS
+            my_map.insert(key, value.clone());
+            &value
         }
     };
 
     println!("{}", res);
 }
 
+
+
 // Exercise 6
 // Make it compile
 
-use std::io;
+use std::io::{self};
 
-fn exercise6() {
-    let mut prev_key: &str = "";
-
-    for line in io::stdin().lines() {
-        let s = line.unwrap();
-
-        let data: Vec<&str> = s.split("\t").collect();
-        if prev_key.len() == 0 {
-            prev_key = data[0];
-        }
-    }
-}
-
-// Exercise 7
-// Make it compile
-fn exercise7() {
-    let mut v: Vec<&str> = Vec::new();
-    {
-        let chars = [b'x', b'y', b'z'];
-        let s: &str = std::str::from_utf8(&chars).unwrap();
-        v.push(&s);
-    }
-    println!("{:?}", v);
-}
-
-// Exercise 8
-// Make it compile
 fn exercise8() {
-    let mut accounting = vec!["Alice", "Ben"];
-    
+    let mut accounting = vec!["Alice".to_string(), "Ben".to_string()];
+
     loop {
-        let mut add_input = String::from("");
+        let mut add_input = String::new();
 
-        io::stdin()
-            .read_line(&mut add_input)
-            .expect("Failed to read line");
-
-        let add_vec: Vec<&str> = add_input.trim()[..].split_whitespace().collect();
-
-        if add_vec.len() < 1 {
+        if io::stdin().read_line(&mut add_input).expect("Failed to read line") == 0 {
             println!("Incorrect input, try again");
             continue;
         }
 
-        let person = add_vec[0];
+        let add_vec: Vec<&str> = add_input.trim().split_whitespace().collect();
+
+        if add_vec.is_empty() {
+            println!("Incorrect input, try again");
+            continue;
+        }
+
+        let person = add_vec[0].to_string();
         accounting.push(person);
     }
 }
